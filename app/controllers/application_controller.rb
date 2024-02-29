92fa53f2ea3d9bcf867115f565e39c7d30283759
YUJI1027 <soka.miyara_6161@icloud.com>
2024-02-29 15:09:52 +0900

    Commit message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   
-  before_action :authenticate_user!
+  before_action :authenticate_user!,except: :index
   def after_sign_in_path_for(resource)
     tasks_path
   end
