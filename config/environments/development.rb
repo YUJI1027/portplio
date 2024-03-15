8657f6466f3d2ee7557ca05233ae2a8de02e45f5
YUJI1027 <soka.miyara_6161@icloud.com>
2024-03-16 00:28:55 +0900

    demo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,5 +73,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = false
-  config.hosts<<rails-release-app-ccac73256fd8.herokuapp.com
+  config.hosts<<"rails-app-yuji-6c546b1edceb.herokuapp.com"
 end
