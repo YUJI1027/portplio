baca0e6752cdb6f58126ead8bc094fa0280fbdff
YUJI1027 <soka.miyara_6161@icloud.com>
2024-03-18 00:27:31 +0900

    fixed css

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users  
-  root to: 'tasks#new'
+  root to: 'tasks#index'
   resources :tasks, only: [:index, :new, :create, :edit, :update, :destroy]
 
 
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resource :likes, only: [:create, :destroy]
   end
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
